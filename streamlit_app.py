6e4ddb7fe4c5d7aeaecf4a7b5ba31679bc9f3803
elad ambar <eladambar@elads-MacBook-Air.local>
2023-01-05 17:54:21 +0200

    added searchbox and display results for top 3 files

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,32 @@
-from collections import namedtuple
-import altair as alt
-import math
 import pandas as pd
+import numpy as np
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import openai
+from openai.embeddings_utils import cosine_similarity
+from openai.embeddings_utils import get_embedding
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+openai.api_key = "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"
+df2 = pd.read_csv('text-data_with_embeddings.csv')
+df2["ada_search"] = df2["ada_search"].apply(eval).apply(np.array)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+def get_similar_questions(question, df, top_n=3):
+    question_embedding = get_embedding(question, engine='text-embedding-ada-002')
+    df['similarity'] = df.ada_search.apply(lambda x: cosine_similarity(x, question_embedding))
+    return df.sort_values('similarity', ascending=False).head(top_n)
 
-    points_per_turn = total_points / num_turns
+def get_similar_questions_pretty(question, df, top_n=3):
+    values = get_similar_questions(question, df, top_n)
+    return values.doc_title.to_numpy()
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+def get_similar_title(question):
+    return get_similar_questions_pretty(question, df2)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+def search_in_csv(search_term):
+    if search_term == '':
+        return ''
+    return get_similar_questions_pretty(search_term, df2)
+
+
+search_term = st.text_input("Please enter a search phrase:")
+st.write('The results are:', search_in_csv(search_term))
